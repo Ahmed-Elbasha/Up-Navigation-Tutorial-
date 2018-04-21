adbed2b6fe9baef59cca081e041872caca4de82a
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-04-21 11:02:20 +0200

    Add the project files

---
+++ UpNavigationTutorial /app/src/main/java/com/example/ahmedelbasha/upnavigationtutorial/ChildActivity.java
@@ -0,0 +1,46 @@
+package com.example.ahmedelbasha.upnavigationtutorial;
+
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class ChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    // navigating to parent Activity from activity in our app.
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                NavUtils.navigateUpFromSameTask(this);
+//                return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
+
+    // navigating to parent activity with possibility to be navigating back from another app's activity.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent upIntent = NavUtils.getParentActivityIntent(this);
+                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+                    TaskStackBuilder.create(this).addNextIntentWithParentStack(upIntent).startActivities();
+                } else {
+                    NavUtils.navigateUpTo(this, upIntent);
+                }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
